36ab9210423af92352f31dc611bfa55c73bc3278
hojas.adrian <hojas.adrian@gmail.com>
2024-01-07 22:35:31 -0500

    change server mode

--- server.ts
+++ server.ts
@@ -4,15 +4,15 @@ import { bot } from "./src/bot.ts";
 const handleUpdate = webhookCallback(bot, "std/http");
 
 Deno.serve(async (req) => {
-  const path = new URL(req.url).pathname.slice(1);
-
-  if (req.method === "POST" && path === bot.token) {
-    try {
-      return await handleUpdate(req);
-    } catch (err) {
-      console.error(err);
+  if (req.method === "POST") {
+    const url = new URL(req.url);
+    if (url.pathname.slice(1) === bot.token) {
+      try {
+        return await handleUpdate(req);
+      } catch (err) {
+        console.error(err);
+      }
     }
   }
-
-  return Response.redirect(`https://telegram.me/${bot.botInfo.username}`);
+  return new Response();
 });
